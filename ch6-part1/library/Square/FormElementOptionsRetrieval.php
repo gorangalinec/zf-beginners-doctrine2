77978681037e7ff556e7bd2ce15aca6fe2add4cb
Kurt Krueckeberg <kurtk@pobox.com>
2011-09-08 17:00:08 -0500

    added ch6-part1 and retrofitted ch6 form changes into ch5

---
+++ ch6-part1/library/Square/FormElementOptionsRetrieval.php
@@ -0,0 +1,10 @@
+<?php
+namespace Square;
+
+interface FormElementOptionsRetrieval {
+    
+    //--public function getDefaultMultiOptions(\Zend_Form_Element_Multi $element);
+     public function getDefaultMultiOptions($e);
+}
+
+?>
